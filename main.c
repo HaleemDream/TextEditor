94112489f5cdd7a39d5d18fac3c1bd672c2b8afa
Haleem <Haleem.Assal@gmail.com>
2019-10-08 21:40:16 -0300

    Add text area

--- main.c
+++ main.c
@@ -2,15 +2,21 @@
 
 void activate(GtkApplication* app, gpointer user_data){
     GtkWidget* window;
+    GtkWidget* text_area;
 
+    //main window
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Dream Editor");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+
+    //text area
+    text_area = gtk_text_view_new();
+    gtk_container_add(GTK_CONTAINER(window), text_area);
+
     gtk_widget_show_all(window);
 }
 
 int main(int argc, char **argv){
-
     GtkApplication* app;
     int status;
 
