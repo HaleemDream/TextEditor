0a78771306bf270b8ce8dec04c16d59484b5964b
Haleem <Haleem.Assal@gmail.com>
2019-10-12 14:32:30 -0300

    convert to menu items

--- menu/main_menu.c
+++ menu/main_menu.c
@@ -9,13 +9,15 @@ GMenu* g_main_menu(){
     GMenu* file_menu = g_menu_new();
 
     //file menu children
+    GMenuItem* open = g_menu_item_new("Open", "Open");
+    GMenuItem* save = g_menu_item_new("Save", "Save");
     GMenuItem* close = g_menu_item_new("Close", "Close");
 
     //set attr
     g_menu_item_set_attribute(close, G_MENU_ATTRIBUTE_ACTION, "s", "app.close");
 
-    g_menu_insert(file_menu, 0, "Open", "Open");
-    g_menu_insert(file_menu, 1, "Save", "Save");
+    g_menu_insert_item(file_menu, 0, open);
+    g_menu_insert_item(file_menu, 1, save);
     g_menu_insert_item(file_menu, 2, close);
     
     //about menu
