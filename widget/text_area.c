8025bc4c34ff2d0903959ebf2380d62a4a4ad5a1
Haleem <Haleem.Assal@gmail.com>
2019-10-13 05:08:26 -0300

    Add wrap on char

--- widget/text_area.c
+++ widget/text_area.c
@@ -3,6 +3,7 @@
 GtkWidget* get_text_area(){
     if(text_area == NULL){
         text_area = gtk_text_view_new();
+        gtk_text_view_set_wrap_mode((GtkTextView*) text_area, GTK_WRAP_CHAR);
     }
 
     return text_area;
